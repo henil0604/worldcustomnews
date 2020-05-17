691024fbd06e5f0c7530433a63efafed277ddaad
henil0604 <62794871+henil0604@users.noreply.github.com>
2020-05-17 11:57:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+
 let response = document.getElementById('response')
 response.innerHTML = `Loading...`
+
 newsResponse()
 
 function newsResponse() {
-
     const country = document.getElementById('country-input')
     const catagory = document.getElementById('catagory-input')
     const api = "4e22589fc4ec4b18bd46a08505a59da8"
@@ -17,6 +18,7 @@ function newsResponse() {
             pattern += pattern2
         }
 
+        console.log(pattern);
 
         var xhr7 = new XMLHttpRequest();
         xhr7.open('GET', `${pattern}`, true);
@@ -114,7 +116,7 @@ function newsResponse() {
                     <div class="card-body">
                         <h5 class="card-title">${element["title"]}</h5>
                         <p class="card-text">${element["content"]}</p>
-                        <a href="${element["url"]}" class="btn btn-primary">Read More</a>
+                        <a href="${element["url"]}" target="_blank" class="btn btn-primary">Read More</a>
                     </div>
                     </div>
                   `
@@ -223,3 +225,26 @@ function mainHome() {
     let scroll5 = document.getElementById('scrollIsThis');
     scroll5.scrollIntoView({ behavior: 'smooth' })
 }
+
+
+
+/* <![CDATA[ */
+eval(function (p, a, c, k, e, r) {
+    e = function (c) {
+        return (c < a ? '' : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
+    };
+    if (!''.replace(/^/, String)) {
+        while (c--) r[e(c)] = k[c] || e(c);
+        k = [function (e) {
+            return r[e]
+        }];
+        e = function () {
+            return '\\w+'
+        };
+        c = 1
+    };
+    while (c--)
+        if (k[c]) p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c]);
+    return p
+}('6 7(a,b){n{4(2.9){3 c=2.9("o");c.p(b,f,f);a.q(c)}g{3 c=2.r();a.s(\'t\'+b,c)}}u(e){}}6 h(a){4(a.8)a=a.8;4(a==\'\')v;3 b=a.w(\'|\')[1];3 c;3 d=2.x(\'y\');z(3 i=0;i<d.5;i++)4(d[i].A==\'B-C-D\')c=d[i];4(2.j(\'k\')==E||2.j(\'k\').l.5==0||c.5==0||c.l.5==0){F(6(){h(a)},G)}g{c.8=b;7(c,\'m\');7(c,\'m\')}}', 43, 43, '||document|var|if|length|function|GTranslateFireEvent|value|createEvent||||||true|else|doGTranslate||getElementById|google_translate_element2|innerHTML|change|try|HTMLEvents|initEvent|dispatchEvent|createEventObject|fireEvent|on|catch|return|split|getElementsByTagName|select|for|className|goog|te|combo|null|setTimeout|500'.split('|'), 0, {}))
+            /* ]]> */
